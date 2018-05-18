20a82ed7019b94460a9c91e0dfbc534363f08beb
GrapeGreen <andy@spdg.net>
2018-05-18 18:43:40 +0300

    Update mistakes.py

--- mistakes.py
+++ mistakes.py
@@ -1,7 +1,9 @@
 from collections import defaultdict
+from levenshtein import Operation
 import re
 
 d = defaultdict(float)
+types = defaultdict(lambda : defaultdict(tuple))
 
 total = 0
 
@@ -14,6 +16,25 @@ with open('mistakes.txt', 'r') as f:
         old, new = re.split('[|]', line[0])
         d[(old, new)] += freq
         total += freq
+
+        TYPE = Operation.NONE
+
+        if len(old) > len(new):
+            TYPE = Operation.DELETE if old[0] != old[-1] or len(old) == 1 else Operation.SINGLE
+        elif len(old) < len(new):
+            TYPE = Operation.ADD if new[0] != new[-1] or len(new) == 1 else Operation.DOUBLE
+        else:
+            if len(old) > 1 and old[0] == new[-1] and old[-1] == new[0]:
+                TYPE = Operation.TRANSPOSE
+            else:
+                TYPE = Operation.SUBSTITUTE
+
+        types[TYPE][(old, new)] = freq
+
+
+for op in types:
+    print(op)
+    print(types[op])
 
 for key in d:
     d[key] /= total
